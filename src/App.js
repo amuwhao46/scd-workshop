1bc285ac0ed75c92c8b383b6ea464758f2a5a49e
Oke Amuwha <amuwhao46@gmail.com>
2022-11-28 17:31:32 -0500

    Practice useState

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const welcome = <p>Welcome to the front-end</p>;
+  const [count, setCount] = useState(0)
+  const increment = () => {setCount(count + 1)}
+  const decrement = () => {setCount(count - 1)}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={decrement}>-</button>
+      {count}
+      <button onClick={increment}>+</button>
     </div>
   );
 }
