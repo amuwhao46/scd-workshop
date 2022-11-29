c80ab02be64719d6c65f9aab8e71e3e0f6461876
Oke Amuwha <amuwhao46@gmail.com>
2022-11-28 21:52:25 -0500

    Added info for BMI calculator

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
 
 function App() {
-  // useState
-  const [count, setCount] = useState(0)
-  const increment = () => {setCount(count + 1)}
-  const decrement = () => {setCount(count - 1)}
+// Calc BMI (weight (lbs), height (inches))
+  const weightInput = useRef(null);
+  const heightInFeet = useRef(null);
+  const heightInInches = useRef(null);
+  const calcBMI = (weight, height) => {}
 
-  // useEffect
-  useEffect(() => {
-    console.log("Count State Change")
-  }, [count])
+  const handClick = () => {
+    let weight = weightInput.current?.value
+    let heightFt = heightInFeet.current?.value
+    let HeightIn = heightInInches.current?.value
+  }
+
   return (
     <div>
-      <button onClick={decrement}>-</button>
-      {count}
-      <button onClick={increment}>+</button>
+      <h1>Calculate BMI</h1>
+      <input type="number" ref={weightInput} placeholder="Weight (lbs)" />
+      <div style={{display: "inline-block"}}>
+        <input type="number" ref={heightInFeet} placeholder="Height (feet)" />
+        <input type="number" ref={heightInInches} placeholder="Height (inches)" />
+      </div>
     </div>
   );
 }
